4c1195b5b0b36125fc4e7a78346000bbba262f74
Yu Zhao <yuzhao@google.com>
2021-04-05 04:17:41 -0600

    BACKPORT: FROMLIST: mm: multigenerational lru: mm_struct list
    In order to scan page tables, we add an infrastructure to maintain
    either a system-wide mm_struct list or per-memcg mm_struct lists, and
    track whether an mm_struct is being used or has been used since the
    last scan.
    
    Multiple threads can concurrently work on the same mm_struct list, and
    each of them will be given a different mm_struct belonging to a
    process that has been scheduled since the last scan.
    
    Signed-off-by: Yu Zhao <yuzhao@google.com>
    Tested-by: Konstantin Kharlamov <Hi-Angel@yandex.ru>
    (am from https://lore.kernel.org/patchwork/patch/1432184/)
    
    BUG=b:123039911
    TEST=Built
    
    Change-Id: I25d9eda8c6bdc7c3653b9f210a159d6c247c81e8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/2928253
    Reviewed-by: Yu Zhao <yuzhao@chromium.org>
    Reviewed-by: Sonny Rao <sonnyrao@chromium.org>
    Tested-by: Yu Zhao <yuzhao@chromium.org>
    Commit-Queue: Yu Zhao <yuzhao@chromium.org>
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- include/linux/memcontrol.h
+++ include/linux/memcontrol.h
@@ -156,14 +156,7 @@ enum memcg_kmem_state {
 	KMEM_ONLINE,
 };
 
-#if defined(CONFIG_SMP)
-struct memcg_padding {
-	char x[0];
-} ____cacheline_internodealigned_in_smp;
-#define MEMCG_PADDING(name)      struct memcg_padding name;
-#else
-#define MEMCG_PADDING(name)
-#endif
+struct lru_gen_mm_list;
 
 /*
  * The memory controller data structure. The memory controller controls both
@@ -280,6 +273,10 @@ struct mem_cgroup {
 	/* List of events which userspace want to receive */
 	struct list_head event_list;
 	spinlock_t event_list_lock;
+
+#ifdef CONFIG_LRU_GEN
+	struct lru_gen_mm_list *mm_list;
+#endif
 
 	struct mem_cgroup_per_node *nodeinfo[0];
 	/* WARNING: nodeinfo must be the last member here */
@@ -741,6 +738,14 @@ static inline void memcg_memory_event_mm(struct mm_struct *mm,
 void mem_cgroup_split_huge_fixup(struct page *head);
 #endif
 
+struct mem_cgroup *get_mem_cgroup_from_mm(struct mm_struct *mm);
+
+static inline void mem_cgroup_put(struct mem_cgroup *memcg)
+{
+	if (memcg)
+		css_put(&memcg->css);
+}
+
 #else /* CONFIG_MEMCG */
 
 #define MEM_CGROUP_ID_SHIFT	0
@@ -1008,6 +1013,15 @@ static inline void count_memcg_page_event(struct page *page,
 
 static inline
 void count_memcg_event_mm(struct mm_struct *mm, enum vm_event_item idx)
+{
+}
+
+static inline struct mem_cgroup *get_mem_cgroup_from_mm(struct mm_struct *mm)
+{
+	return NULL;
+}
+
+static inline void mem_cgroup_put(struct mem_cgroup *memcg)
 {
 }
 #endif /* CONFIG_MEMCG */
