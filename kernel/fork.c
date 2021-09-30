2df359851928fd4a69c42bed49add7596362e470
Cyber Knight <cyberknight755@gmail.com>
2021-09-30 20:38:12 +0800

    kernel: Boost DDR bus according to set kernel profile
    Level 1 (battery) = no boost
    Level 2 (balanced) = boost for 25ms
    Level 0 (disabled) / Level 3 (performance) = boost for 50ms
    
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- kernel/fork.c
+++ kernel/fork.c
@@ -96,8 +96,9 @@
 #include <linux/scs.h>
 #include <linux/simple_lmk.h>
 #include <linux/cpu_input_boost.h>
-#include <linux/devfreq_boost.h>
+#include <linux/kprofiles.h>
 
+#include <linux/devfreq_boost.h>
 #include <asm/pgtable.h>
 #include <asm/pgalloc.h>
 #include <linux/uaccess.h>
@@ -2234,10 +2235,14 @@ long _do_fork(unsigned long clone_flags,
 	int trace = 0;
 	long nr;
 
-	/* Boost DDR bus to the max for 50 ms when userspace launches an app */
-	if (task_is_zygote(current) && df_boost_within_input(1500))
-		cpu_input_boost_kick_max(50);
-		devfreq_boost_kick_max(DEVFREQ_CPU_LLCC_DDR_BW, 50);
+	/* Boost DDR bus to the max when userspace launches an app according to set kernel profile */
+	if (task_is_zygote(current) && (active_mode() == 2)) {
+	  devfreq_boost_kick_max(DEVFREQ_CPU_LLCC_DDR_BW, 25);
+	} else if (task_is_zygote(current) && ((active_mode() == 3) || (active_mode() == 0))) {
+	  devfreq_boost_kick_max(DEVFREQ_CPU_LLCC_DDR_BW, 50);
+	} else if (task_is_zygote(current) && (active_mode() == 1)) {
+	  pr_info("Battery profile detected! Skipping DDR bus boost...\n");
+	}
 
 	/*
 	 * Determine whether and which event to report to ptracer.  When
