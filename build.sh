0b50cc27429942ca3c35fb847eccf415bcb3fb5b
“Patriot-06” <mbmc172@gmail.com>
2022-03-08 20:59:04 +0000

    build.sh: Fix typo

--- build.sh
+++ build.sh
@@ -32,7 +32,7 @@ make O=out ARCH=arm64 $DEFCONFIG
 
 echo -e "\nStarting compilation...\n"
 make -j$(nproc --all) O=out ARCH=arm64 CC=clang CLANG_TRIPLE=aarch64-linux-gnu- LD=ld.lld AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip CROSS_COMPILE=aarch64-linux-gnu- CROSS_COMPILE_ARM32=arm-linux-gnueabi- Image.gz dtbo.img
-i
+
 kernel="out/arch/arm64/boot/Image.gz"
 dtb="out/arch/arm64/boot/dts/qcom/sm8150-v2.dtb"
 dtbo="out/arch/arm64/boot/dtbo.img"
